f8e575d073207731691a418d0e8193d22477f037
patrykjachym <jachympatryk99@gmail.com>
2023-10-30 15:03:58 +0100

    add formik

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import {
   MapContainer,
   TileLayer,
@@ -11,6 +11,29 @@ import 'leaflet/dist/leaflet.css'
 import * as XLSX from 'xlsx'
 import { Modal, Button, Input, Form } from 'antd'
 import { saveAs } from 'file-saver'
+
+import * as yup from 'yup'
+
+const circleSchema = yup.object().shape({
+  lat: yup
+    .number()
+    .required('Szerokość geograficzna jest wymagana')
+    .min(-90, 'Minimalna szerokość to -90')
+    .max(90, 'Maksymalna szerokość to 90'),
+  lng: yup
+    .number()
+    .required('Długość geograficzna jest wymagana')
+    .min(-180, 'Minimalna długość to -180')
+    .max(180, 'Maksymalna długość to 180'),
+  radius: yup
+    .number()
+    .required('Promień jest wymagany')
+    .min(0, 'Promień nie może być ujemny'),
+  gmv: yup
+    .number()
+    .required('GMV jest wymagane')
+    .min(0, 'GMV nie może być ujemne'),
+})
 
 interface CircleData {
   lat: number
@@ -41,6 +64,7 @@ interface ExcelData {
 
 const Map: React.FC = () => {
   const fileInputRef = useRef<HTMLInputElement>(null)
+  const fileInputExcelRef = useRef<HTMLInputElement>(null)
 
   const [circles, setCircles] = useState<CircleData[]>([])
   const [minGMV, setMinGMV] = useState<number>(Infinity)
@@ -84,7 +108,10 @@ const Map: React.FC = () => {
           const text = e.target?.result
           if (typeof text === 'string') {
             const importedCircles: CircleData[] = JSON.parse(text)
-            setCircles(importedCircles)
+
+            const newCircles = [...circles, ...importedCircles]
+
+            setCircles(newCircles)
 
             const gmvValues = importedCircles.map(circle => circle.gmv)
             setMinGMV(Math.min(...gmvValues))
@@ -96,14 +123,18 @@ const Map: React.FC = () => {
       }
       reader.readAsText(file)
     }
+
+    if (fileInputRef.current) {
+      fileInputRef.current.value = ''
+    }
   }
 
-  const handleAddCircle = () => {
+  const handleAddCircle = (values: CircleForm) => {
     const newCircle = {
-      lat: parseFloat(circleForm.lat),
-      lng: parseFloat(circleForm.lng),
-      radius: parseFloat(circleForm.radius) * 1000, // Konwersja km na metry
-      gmv: parseFloat(circleForm.gmv),
+      lat: parseFloat(values.lat),
+      lng: parseFloat(values.lng),
+      radius: parseFloat(values.radius) * 1000, // Konwersja km na metry
+      gmv: parseFloat(values.gmv),
     }
 
     setCircles([...circles, newCircle])
@@ -112,13 +143,13 @@ const Map: React.FC = () => {
     const gmvValues = circles.map(circle => circle.gmv)
     setMinGMV(Math.min(newCircle.gmv, ...gmvValues))
     setMaxGMV(Math.max(newCircle.gmv, ...gmvValues))
-    setIsModalOpen(false)
     setCircleForm({
       lat: '',
       lng: '',
       radius: '',
       gmv: '',
     })
+    setIsModalOpen(false)
   }
 
   const handleFileUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -127,32 +158,44 @@ const Map: React.FC = () => {
     if (file) {
       const reader = new FileReader()
       reader.onload = (e: any) => {
-        const data = new Uint8Array(e.target.result)
-        const workbook = XLSX.read(data, { type: 'array' })
-        const worksheet = workbook.Sheets[workbook.SheetNames[0]]
-        const json: ExcelData[] = XLSX.utils.sheet_to_json(worksheet, {
-          raw: false,
-        })
+        try {
+          const data = new Uint8Array(e.target.result)
+          const workbook = XLSX.read(data, { type: 'array' })
+          const worksheet = workbook.Sheets[workbook.SheetNames[0]]
+          const json: ExcelData[] = XLSX.utils.sheet_to_json(worksheet, {
+            raw: false,
+          })
 
-        const formattedData: CircleData[] = json.map(item => {
-          const gmv = parseFloat(item.GMV)
-          return {
-            lat: parseFloat(item.Latitude),
-            lng: parseFloat(item.Longitude),
-            radius: parseFloat(item['Bubble radius (km)']) * 1000, // Konwersja km na metry
-            gmv: isNaN(gmv) ? 0 : gmv,
-          }
-        })
+          const formattedData: CircleData[] = json.map(item => {
+            const gmv = parseFloat(item.GMV)
+            return {
+              lat: parseFloat(item.Latitude),
+              lng: parseFloat(item.Longitude),
+              radius: parseFloat(item['Bubble radius (km)']) * 1000, // Konwersja km na metry
+              gmv: isNaN(gmv) ? 0 : gmv,
+            }
+          })
 
-        const gmvValues = formattedData.map(item => item.gmv)
-        const minVal = Math.min(...gmvValues)
-        const maxVal = Math.max(...gmvValues)
+          const gmvValues = formattedData.map(item => item.gmv)
+          const minVal = Math.min(...gmvValues)
+          const maxVal = Math.max(...gmvValues)
 
-        setMinGMV(minVal)
-        setMaxGMV(maxVal)
-        setCircles(formattedData)
+          setMinGMV(minVal)
+          setMaxGMV(maxVal)
+
+          const newCircles = [...circles, ...formattedData]
+
+          setCircles(newCircles)
+        } catch (error) {
+          console.error('Wystąpił błąd podczas wczytywania pliku', error)
+        }
       }
+
       reader.readAsArrayBuffer(file)
+    }
+
+    if (fileInputExcelRef.current) {
+      fileInputExcelRef.current.value = ''
     }
   }
 
@@ -184,6 +227,12 @@ const Map: React.FC = () => {
       setMaxGMV(-Infinity)
     }
   }
+
+  useEffect(() => {
+    if (isInfoModalOpen) {
+      if (circles.length === 0) setIsInfoModalOpen(false)
+    }
+  }, [circles])
 
   return (
     <div className={styles.mapContainer}>
@@ -218,12 +267,16 @@ const Map: React.FC = () => {
           type="file"
           onChange={handleFileUpload}
           style={{ display: 'none' }}
+          ref={fileInputExcelRef}
         />
         <label htmlFor="file-upload" className={styles.fileInput}>
           Wybierz plik
         </label>
         <Button onClick={() => setIsModalOpen(true)}>Dodaj okrąg</Button>
-        <Button onClick={() => setIsInfoModalOpen(true)}>
+        <Button
+          disabled={circles.length === 0}
+          onClick={() => setIsInfoModalOpen(true)}
+        >
           Pokaż informacje o okręgach
         </Button>
 
@@ -237,48 +290,102 @@ const Map: React.FC = () => {
           onChange={importCircles}
           accept=".json"
         />
-        <Button onClick={exportCircles}>Eksportuj Okręgi</Button>
+        <Button disabled={circles.length === 0} onClick={exportCircles}>
+          Eksportuj Okręgi
+        </Button>
       </div>
+
       <Modal
         title="Dodaj okrąg"
         visible={isModalOpen}
         onCancel={() => setIsModalOpen(false)}
         footer={null}
       >
-        <Form layout="vertical">
-          <Form.Item label="Szerokość geograficzna:">
-            <Input
-              name="lat"
-              value={circleForm.lat}
-              onChange={handleInputChange}
-            />
+        <Form
+          layout="vertical"
+          onFinish={handleAddCircle}
+          initialValues={{
+            lat: '',
+            lng: '',
+            radius: '',
+            gmv: '',
+          }}
+        >
+          <Form.Item
+            label="Szerokość geograficzna:"
+            name="lat"
+            rules={[
+              {
+                required: true,
+                message: 'Proszę wprowadzić szerokość geograficzną!',
+              },
+              {
+                type: 'number',
+                message: 'Proszę wprowadzić poprawną liczbę!',
+                transform: value => parseFloat(value),
+              },
+            ]}
+          >
+            <Input type="number" />
           </Form.Item>
-          <Form.Item label="Długość geograficzna:">
-            <Input
-              name="lng"
-              value={circleForm.lng}
-              onChange={handleInputChange}
-            />
+          <Form.Item
+            label="Długość geograficzna:"
+            name="lng"
+            rules={[
+              {
+                required: true,
+                message: 'Proszę wprowadzić długość geograficzną!',
+              },
+              {
+                type: 'number',
+                message: 'Proszę wprowadzić poprawną liczbę!',
+                transform: value => parseFloat(value),
+              },
+            ]}
+          >
+            <Input type="number" />
           </Form.Item>
-          <Form.Item label="Promień (w km):">
-            <Input
-              name="radius"
-              value={circleForm.radius}
-              onChange={handleInputChange}
-            />
+          <Form.Item
+            label="Promień (w km):"
+            name="radius"
+            rules={[
+              {
+                required: true,
+                message: 'Proszę wprowadzić promień!',
+              },
+              {
+                type: 'number',
+                message: 'Proszę wprowadzić liczbę dodatnią!',
+                transform: value => parseFloat(value),
+                min: 0,
+              },
+            ]}
+          >
+            <Input type="number" />
           </Form.Item>
-          <Form.Item label="GMV:">
-            <Input
-              name="gmv"
-              value={circleForm.gmv}
-              onChange={handleInputChange}
-            />
+          <Form.Item
+            label="GMV:"
+            name="gmv"
+            rules={[
+              {
+                required: true,
+                message: 'Proszę wprowadzić GMV!',
+              },
+              {
+                type: 'number',
+                message: 'Proszę wprowadzić poprawną liczbę!',
+                transform: value => parseFloat(value),
+              },
+            ]}
+          >
+            <Input type="number" />
           </Form.Item>
-          <Button type="primary" onClick={handleAddCircle}>
+          <Button type="primary" htmlType="submit">
             Dodaj
           </Button>
         </Form>
       </Modal>
+
       <Modal
         title="Informacje o okręgach"
         visible={isInfoModalOpen}
