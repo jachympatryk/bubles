034a46e86dce064cb32f80a2cdb39fe560fd9b31
patrykjachym <jachympatryk99@gmail.com>
2023-10-30 10:20:11 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import './App.css'
-import Map from './components/map'
+// import Map from './components/map'
 
 function App() {
   return (
     <div className="App">
-      <Map />
+      {/*<Map />*/}
+      <div>test test</div>
     </div>
   )
 }
